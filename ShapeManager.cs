213b7051a8b0137fe0c04afb520994dd8d828970
ABDELRAHMAN AHMED <75867889+Abdo1429@users.noreply.github.com>
2024-05-12 23:03:23 +0200

    Add files via upload

---
+++ ShapeManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CADApplication
+{
+    public class ShapeManager
+    {
+        private List<Shape> shapes = new List<Shape>();
+
+        public void AddShape(Shape shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void ListAllShapes()
+        {
+            foreach (var shape in shapes)
+            {
+                shape.DisplayInfo();
+            }
+        }
+    }
+}
